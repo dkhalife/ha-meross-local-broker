00ea5d7087aaeeb32a249d92cca022e04f33f04b
Dany Khalife <github@dkhalife.com>
2024-01-07 12:29:40 -0800

    make connection params configuration options

--- meross_local_broker/rootfs/usr/local/bin/broker.sh
+++ meross_local_broker/rootfs/usr/local/bin/broker.sh
@@ -14,8 +14,15 @@ else
   debug=""
 fi
 
+# Setup MQTT connection params
+MQTT_HOST=$(get_option 'host' '127.0.0.1')
+MQTT_PORT=$(get_option 'port' '8883')
+AGENT_USERNAME=$(get_option 'username' '')
+AGENT_PASSWORD=$(get_option 'password' '')
+CA_CERT=$(get_option 'ca_cert' '/data/ca.crt')
+
 # Wait until mqtt is ready is available
 bashio::log.info "Waiting MQTT server..."
-bashio::net.wait_for 2001
+bashio::net.wait_for $MQTT_PORT
 
-exec python3 broker_agent.py --port 2001 --host localhost --username "$AGENT_USERNAME" --password "$AGENT_PASSWORD" --cert-ca "/data/mqtt/certs/ca.crt" $debug
+exec python3 broker_agent.py --port $MQTT_PORT --host "$MQTT_HOST" --username "$AGENT_USERNAME" --password "$AGENT_PASSWORD" --cert-ca "$CA_CERT" $debug
