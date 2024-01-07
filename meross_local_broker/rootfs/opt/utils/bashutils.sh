9963e7f6136fb3bda314581eca84c0f4bcea3cfd
Dany Khalife <github@dkhalife.com>
2024-01-07 12:52:57 -0800

    restore options parsing logic

--- meross_local_broker/rootfs/opt/utils/bashutils.sh
+++ meross_local_broker/rootfs/opt/utils/bashutils.sh
@@ -4,6 +4,13 @@ function get_option {
     parameter_name=$1
     default_value=$2
 
+    # Give precedence to options file, if set.
+    options_value=$(jq -e --raw-output ".$parameter_name" $CONFIG_PATH)
+    if [[ $? -eq 0 ]]; then
+        echo "$options_value"
+        return 
+    fi
+
     # Look if an env var is set for that parameter_name
     env_value=$(printenv "$parameter_name")
     if [[ $? -eq 0 ]]; then
